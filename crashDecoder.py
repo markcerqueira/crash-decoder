c46f236be07f88c538f10850a5f8019d2e7b2243
Mark Cerqueira <mark.cerqueira@gmail.com>
2013-05-26 13:19:34 +0900

    Cleanup

--- crashDecoder.py
+++ crashDecoder.py
@@ -24,28 +24,3 @@ subprocess.call( './HexToBinary ' + str(sys.argv[1]) + '_hex ' + str(sys.argv[1]
 
 # process crash report
 subprocess.call('./plcrashutil convert --format=iphone ' + str(sys.argv[1]) + '_binary > ' + str(sys.argv[1]) + "_crash" , shell=True )
-
-#crashReport = open(str(sys.argv[1]) + "_crash", 'r')
-
-#appName = ""
-
-# find the app name
-#for line in crashReport:
-#    m = re.search( '^Identifier:', line)
-#    if (m != None):
-#        appName = line.replace('Identifier:', '')
-#        appName = appName.replace(' ', '')
-#        appName = appName.rstrip()
-#        break
-
-# give crash report more meaningful name
-#crashReportName = appName + "_" + str(time.localtime().tm_mon) + "_" + str(time.localtime().tm_mday)
-
-#command = 'mv ' + str(sys.argv[1]) + "_crash " + crashReportName
-
-#print command
-
-#subprocess.call(command)
-
-# clean up files
-#subprocess.Popen( 'rm ' + str(sys.argv[1]) + '_hex ' + str(sys.argv[1]) + '_binary', shell=True )
